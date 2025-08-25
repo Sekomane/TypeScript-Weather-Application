229c3b479fc8edad74b37c4f36738958ad79faf9
Sekomane <sekomanerorisang904@gmail.com>
2025-08-26 01:24:09 +0200

    Ready for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/HomePage'
 import WeatherPage from './components/WeatherPage'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/weather/:location" element={<WeatherPage />} />
-    </Routes>
+    <BrowserRouter basename="/TypeScript-Weather-Application/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/weather/:location" element={<WeatherPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
